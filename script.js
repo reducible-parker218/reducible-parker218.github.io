95da60274583de2a77be67ca00e0d493c599ba35
Nitish Patil <nitishpatil.in@gmail.com>
2026-07-10 17:12:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function toggleMenu() {
-    document.getElementById("navbar").classList.toggle("active");
+
+const menuBtn=document.querySelector(".menu-btn");
+const nav=document.querySelector("nav");
+
+menuBtn.onclick=()=>{
+
+nav.classList.toggle("active");
+
+if(nav.classList.contains("active")){
+menuBtn.innerHTML='<i class="fas fa-times"></i>';
+}
+else{
+menuBtn.innerHTML='<i class="fas fa-bars"></i>';
+}
+
 }
